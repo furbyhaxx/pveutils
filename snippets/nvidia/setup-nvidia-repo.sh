76aa133d3e205285727f5676a2ba563be2fd8591
furbyhaxx <vital@reichmuth.cc>
2023-10-30 11:24:00 +0100

    nvidia driver and cuda setup

--- snippets/nvidia/setup-nvidia-repo.sh
+++ snippets/nvidia/setup-nvidia-repo.sh
@@ -11,11 +11,20 @@ dpkg -i cuda-keyring_1.1-1_all.deb
 #echo "deb [signed-by=/usr/share/keyrings/cuda-archive-keyring.gpg] https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/ /" | sudo tee /etc/apt/sources.list.d/cuda-ubuntu2204-x86_64.list
 
 # Add pin file to prioritize CUDA repository:
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
-sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+#wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+#sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+echo "> Pin Cuda Repository."
+cat <<EOF | tee /etc/apt/preferences.d/priority-nvidia
+Package: *
+Pin: release o=NVIDIA,l=NVIDIA CUDA
+Pin-Priority: 100
+EOF
+echo
 
+echo "> Updating APT packages."
+sudo apt update
 
-read -p "Do you want to install the latest cuda? (YES/no) " yn
+read -p "Do you want to install the latest nvidia driver and hold it on this version? (YES/no) " yn
 
 case $yn in 
 	yes|YES|y|Y|"" ) echo ok, we will proceed;;
@@ -25,17 +34,36 @@ case $yn in
 	exit 1;
 esac
 
-sudo apt update
+
+apt install -y nvidia-driver
+
+sudo apt-mark hold nvidia-driver
+
+read -p "Do you want to install the latest cuda version? (YES/no) " yn
+
+case $yn in 
+        yes|YES|y|Y|"" ) echo ok, we will proceed;;
+        no|NO|n|N ) echo exiting...;
+                exit;;
+        * ) echo wrong input, exiting;
+        exit 1;
+esac
+
 
 # Install CUDA SDK:
-sudo apt install -y cuda-toolkit
+#sudo apt install -y cuda-toolkit
+sudo apt install -y cuda-toolkit-12-3
 
 # Install CUDA GDS
-sudo apt install -y nvidia-gds
+#sudo apt install -y nvidia-gds
 
 # Alter PATH
-export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+echo "> Set environment variables."
+cat <<EOF | tee /etc/environment
+PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/cuda/bin"
+LD_LIBRARY_PATH="/usr/local/cuda/lib64"
+echo
+
 
 echo "Done"
 
